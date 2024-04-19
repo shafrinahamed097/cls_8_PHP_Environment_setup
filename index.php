c971cc65fefa6f6b0179a4003a2e2d59e494e159
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-04-19 10:48:45 +0600

    Declare function & Define Const, Access Define

--- index.php
+++ index.php
@@ -6,6 +6,46 @@ $c=$a*$b;
 
 echo $c;
 
+$name="Shafrin Ahamed"; //String
+$age=27; // Integer Numbers
+$isBangladeshi=true; // Boolean
+$weight=65;
+$g=null;
+$h=array('Bangladesh', 'Canada'); //Array
+
+// Scope 1. Local 2.Global 3. Super Global $_SESSION $_GET $_POST
+
+
+// Global Scope
+function myFun1(){
+    global $name;
+ echo  $name;
+}
+
+
+// Local Scope
+function myFun2(){
+  $name="Mr.Shakib";
+  echo $name;
+}
+
+myFun1();
+myFun2();
+
+
+// Const Define
+
+define('BATCH', '3');
+define('SITE_NAME', 'http://ostad.app');
+
+
+
+// Const Access
+
+echo BATCH;
+echo SITE_NAME;
+
+
 
 
 
